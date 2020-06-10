4d7ffaaa71e26ce7ab4a4b4b590732ab50b0f888
Sadhana D <sadhana@hyd.pllcsoftwaresolutions.com>
2020-06-10 11:50:07 +0530

    Fixed q3.js

--- q3.js
+++ q3.js
@@ -3,7 +3,7 @@ const request=require('request');
 
 request({
     method:'GET',
-    url:'http://webcode.me'
+	url:'http://zetcode.com/javascript/cheerio/'
     },(err,res,body)=>{
         if(err) return console.log(err);
         let $=cheerio.load(body);
@@ -14,8 +14,12 @@ request({
            return el.name=='meta' && el.type=='tag' ;
         });
         let items = filteredEls.get();
+		console.log('Meta tags Count :'+ items.length);
+		let i=1;
         items.forEach(e => {
-            console.log(e.name);
+			if(e.attribs.name==undefined){console.log(i+")Meta Desceiption is Empty");}
+			else{console.log(i+')'+e.attribs.name);}   
+			i++;			
         });
     }
 )
